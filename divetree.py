307e5584caa03e1073a18a9886c2f3a23af19aae
Fabio <fabio.giulitti@gmail.com>
2024-06-25 07:50:38 +0200

    add tree levels

--- divetree.py
+++ divetree.py
@@ -5,5 +5,17 @@ import divemongo
 class DiveTree(QTreeView):
     def __init__(self, parent: QWidget | None = ...) -> None:
         super().__init__(parent)
-        self.setModel(divemongo.create_mongo_model())
+        self.setModel(self.createBaseModel())
         self.show()
+
+    def createBaseModel(self):
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(['Connections'])
+        model.appendRow(self.getConnections())
+        return model
+
+    def getConnections(self):
+        mongoItem = QStandardItem("Localhost Mongo")
+        for item in divemongo.create_mongo_model():
+            mongoItem.appendRow(item)
+        return mongoItem
