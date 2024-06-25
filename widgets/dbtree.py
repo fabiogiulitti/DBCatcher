c16712331dbed26fba05a963827c8698a6091ad0
Fabio <fabio.giulitti@gmail.com>
2024-06-25 07:52:36 +0200

    wip isolate model definition for QTreeView

--- widgets/dbtree.py
+++ widgets/dbtree.py
@@ -1,11 +1,8 @@
 from PyQt6.QtWidgets import QTreeView
-from PyQt6.QtGui import QStandardItemModel, QStandardItem
 from PyQt6.QtCore import Qt, QModelIndex
-from core.treepath import Node
-from configyaml import config as conf
-from widgets.utils import createItem
 from widgets.dbcontent import DbContent
-from core.manager import getNav,getAction
+from core.manager import getAction
+from widgets.modelmanager import ModelManager
 
 class DbTree(QTreeView):
 
@@ -13,30 +10,11 @@ class DbTree(QTreeView):
 
     def __init__(self, parent: QTreeView | None = ...) -> None:
         super().__init__(parent)
-        self.setModel(self.createBaseModel())
+        self.modelManager = ModelManager.createBaseModel()
+        self.setModel(self.modelManager.getModel())
         self.expanded.connect(self.on_item_expanded)
         self.show()
 
-    def createBaseModel(self) -> QStandardItemModel:
-        model = QStandardItemModel()
-        model.setHorizontalHeaderLabels(['Connections'])
-        rootItem = model.invisibleRootItem()
-        rootItem.appendRows(self.getConnections())
-        return model
-
-    def getConnections(self):
-        connectionItems = list()
-
-        for connection in conf.get_value("connections"):
-            name = connection['name']
-            type = connection['type']
-            uri = connection['connectionURI']
-            mongoItem = QStandardItem(f"{name} {type}")
-            mongoItem.setData({'levelTag' : 'connections'
-            ,'connectionURI' : uri})
-            mongoItem.appendRow(QStandardItem('(LOADING)'))
-            connectionItems.append(mongoItem)
-        return connectionItems
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
@@ -55,12 +33,4 @@ class DbTree(QTreeView):
         
 
     def on_item_expanded(self, index: QModelIndex):
-        item = self.model().itemFromIndex(index)
-        data = index.model().itemData(index)
-        node: Node = getNav(data[257])
-        self.addNodes(item,node)
-
-    def addNodes(self, parent: QStandardItem, node: Node):
-        parent.removeRow(0)
-        parent.appendRows(map(lambda i: createItem(parent.data(), i, node), node.items))
-
+        self.modelManager.expandModel(index)
---
+++ widgets/modelmanager.py
@@ -0,0 +1,49 @@
+from PyQt6.QtGui import QStandardItem, QStandardItemModel
+from configyaml import config as conf
+from core.manager import getNav
+from core.treepath import Node
+from widgets.utils import createItem
+
+class ModelManager:
+    
+
+    @staticmethod
+    def createBaseModel() -> QStandardItemModel:
+        model = QStandardItemModel()
+        model.setHorizontalHeaderLabels(['Connections'])
+        rootItem = model.invisibleRootItem()
+        rootItem.appendRows(getConnections())
+        manager = ModelManager()
+        manager.model = model
+        return manager
+    
+    def getModel(self):
+         return self.model
+
+
+    def expandModel(self, index):
+        item = self.model.itemFromIndex(index)
+        data = index.model().itemData(index)
+        node: Node = getNav(data[257])
+        addNodes(item,node)
+
+        
+def addNodes(parent: QStandardItem, node: Node):
+        parent.removeRow(0)
+        parent.appendRows(map(lambda i: createItem(parent.data(), i, node), node.items))
+
+
+def getConnections():
+        connectionItems = list()
+        for connection in conf.get_value("connections"):
+            name = connection['name']
+            type = connection['type']
+            uri = connection['connectionURI']
+            mongoItem = QStandardItem(f"{name} {type}")
+            mongoItem.setData({'levelTag' : 'connections'
+            ,'connectionURI' : uri})
+            mongoItem.appendRow(QStandardItem('(LOADING)'))
+            connectionItems.append(mongoItem)
+        return connectionItems
+
+
