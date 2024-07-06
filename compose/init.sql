06bf5d4a76d4f55e721607e5f57ed479c2fe2aec
Fabio <fabio.giulitti@gmail.com>
2024-07-06 12:08:33 +0200

    Add pgsql docker image

---
+++ compose/init.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+
+INSERT INTO users (name, email) VALUES ('John Doe', 'john.doe@example.com');
+INSERT INTO users (name, email) VALUES ('Jane Smith', 'jane.smith@example.com');
+INSERT INTO users (name, email) VALUES ('Alice Johnson', 'alice.johnson@example.com');
+INSERT INTO users (name, email) VALUES ('Bob Brown', 'bob.brown@example.com');
