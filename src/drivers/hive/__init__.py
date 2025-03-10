d8f4391845d4cc096f114aedd819f3d9435f8743
Fabio <fabio.giulitti@gmail.com>
2025-03-10 08:52:07 +0100

    Start writing hive driver

---
+++ src/drivers/hive/__init__.py
@@ -0,0 +1,22 @@
+# Package definition for hive/kyuubi driver
+
+from core.driver.dbdriver import AbstractDbDriver
+from core.ActonTypeEnum import ObjectTypeEnum
+from drivers.postgresql.postgresql.contentactionrules import PSActionDef
+from drivers.postgresql.postgresql.treeactionrules import PSTreeActions
+from drivers.postgresql.postgresql.tabularactionrules import PSTabularActionDef
+from drivers.postgresql.postgresql.queryactionrules import PSQueryActionDef
+
+
+class PostgreSQLDriver(AbstractDbDriver):
+
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._objects[ObjectTypeEnum.TEXT_AREA] = PSActionDef()
+        self._objects[ObjectTypeEnum.DB_TREE] = PSTreeActions()
+        self._objects[ObjectTypeEnum.TABULAR] = PSTabularActionDef()
+        self._objects[ObjectTypeEnum.QUERY_EDIT] = PSQueryActionDef()
+        
+
+    
