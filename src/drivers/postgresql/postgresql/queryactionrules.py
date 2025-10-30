cdde698dfd8f1f2a554a371a94ee370aa129f685
Fabio <fabio.giulitti@gmail.com>
2025-10-30 19:34:25 +0100

    PostgreSQL - Update query enabled

--- src/drivers/postgresql/postgresql/queryactionrules.py
+++ src/drivers/postgresql/postgresql/queryactionrules.py
@@ -5,6 +5,7 @@ from main.core.treepath import references
 from main.core.driver.abstractdriver import AbstractDriver
 from drivers.postgresql.postgresql.treeactionrules import DataResponse
 from main.core.ActonTypeEnum import ActionTypeEnum
+from psycopg2 import connect, extensions
 
 class PSQueryActionDef(AbstractDriver):
 
@@ -27,17 +28,24 @@ def executeQuery(ctx, dim_page=200):
     conn = references[id]['client']
 
     try:
-        cur = conn.cursor()
+        cur: extensions.cursor = conn.cursor()
         query: str = ctx['query']
-        match = re.search(r'\bLIMIT\s+\d+', query, flags=re.IGNORECASE)
-        if match:
-            dim_page = int(match.group().split(' ')[1])
-        else:
-            query = f"{query} LIMIT {dim_page}"
+        match_select = re.search(r'^\s+SELECT\s+', query, flags=re.IGNORECASE)
+        match_limit = re.search(r'\bLIMIT\s+\d+', query, flags=re.IGNORECASE)
+        if match_select:
+            if match_limit:
+                dim_page = int(match_limit.group().split(' ')[1])
+            else:
+                query = f"{query} LIMIT {dim_page}"
 
         cur.execute(query)
-        rows = cur.fetchall()
-        cols = [desc[0] for desc in cur.description]
+        print(f"count {cur.rowcount}")
+        if cur.description is None:
+            rows = [[cur.rowcount]]
+            cols = ["Affected rows"]
+        else:
+            rows = cur.fetchall()
+            cols = [desc[0] for desc in cur.description] #if cur.description else []
         cur.close()
         metadata = ctx.copy()
         metadata['cur_page'] = 0
